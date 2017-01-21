79a2ba4c3354a06f5c4367c941a138d025e0da20
岡大輔 <daisukesegawa@okadaisuke-no-MacBook-Pro-2.local>
2017-01-21 23:22:21 +0900

    株式追加

---
+++ MonkeyPortfolio/MainViewController.swift
@@ -0,0 +1,86 @@
+//
+//  MainViewController.swift
+//  MonkeyPortfolio
+//
+//  Created by 岡大輔 on 2017/01/21.
+//  Copyright © 2017年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+ 
+    @IBAction func pushInvestButton(_ sender: Any) {
+        let stockStore = JapaneseStock20170121()
+        let stockarray = stockStore.returnStockArray()
+        
+        var shuffledArray = stockarray.shuffled()
+        
+        let alert: UIAlertController = UIAlertController(title: "投資案件", message: shuffledArray[0], preferredStyle:  UIAlertControllerStyle.alert)
+        
+        // ② Actionの設定
+        // Action初期化時にタイトル, スタイル, 押された時に実行されるハンドラを指定する
+        // 第3引数のUIAlertActionStyleでボタンのスタイルを指定する
+        // OKボタン
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
+            // ボタンが押された時の処理を書く（クロージャ実装）
+            (action: UIAlertAction!) -> Void in
+            print("OK")
+        })
+        // キャンセルボタン
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.cancel, handler:{
+            // ボタンが押された時の処理を書く（クロージャ実装）
+            (action: UIAlertAction!) -> Void in
+            print("Cancel")
+        })
+        
+        // ③ UIAlertControllerにActionを追加
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        
+        // ④ Alertを表示
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension Array {
+    
+    func shuffled() -> [Element] {
+        var results = [Element]()
+        var indexes = (0 ..< count).map { $0 }
+        while indexes.count > 0 {
+            let indexOfIndexes = Int(arc4random_uniform(UInt32(indexes.count)))
+            let index = indexes[indexOfIndexes]
+            results.append(self[index])
+            indexes.remove(at: indexOfIndexes)
+        }
+        return results
+    }
+    
+}
